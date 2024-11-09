240f5c3028f5b97e51cd3616b775a9cdfa27a223
Kris <kris.shcheglova@bk.ru>
2024-11-09 23:30:54 +0200

    feat: create carousel pricing card

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $('.works__container').slick({
     infinite: true,
     slidesToShow:4,
     slidesToScroll: 1
+});
+$('.pricing__cards').slick({
+    infinite: true,
+    slidesToShow:3,
+    slidesToScroll: 1
   });
